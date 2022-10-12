53f45c4c2c921c9cf58c5bce29e5f826549fa66a
Vaz <the4wonz@outlook.com>
2022-10-13 06:56:31 +0700

    Update plugins.lua
    -- Auto install packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+-- Auto install packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local status, packer = pcall(require, "packer")
 if not status then
 	print("Packer is not installed")
@@ -68,4 +82,5 @@ packer.startup(function(use)
 	-- 		{ "nvim-treesitter/nvim-treesitter" },
 	-- 	},
 	-- })
+  	if packer_bootstrap then require('packer').sync() end
 end)
