f5c2e69d9f7320bb620a4d89bd15ed2c00997cb2
vazw <4wonzest@gmail.com>
2022-10-13 09:07:56 +0700

    some edit

--- nvim/plugin/null-ls.lua
+++ nvim/plugin/null-ls.lua
@@ -1,43 +1,43 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+    return
 end
 
 local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
 null_ls.setup({
-	sources = {
-		-- Python
-		null_ls.builtins.formatting.isort.with({
-			extra_args = { "--line-length", "79", "--profile", "black" },
-		}),
-		null_ls.builtins.formatting.black.with({
-			extra_args = { "--line-length", "79" },
-		}),
-		null_ls.builtins.diagnostics.flake8.with({
-			filetypes = { "python" },
-			extra_args = { "--extend-ignore", "E203, F841" },
-		}),
+    sources = {
+        -- Python
+        null_ls.builtins.formatting.isort.with({
+            extra_args = { "--line-length", "79", "--profile", "black" },
+        }),
+        null_ls.builtins.formatting.black.with({
+            extra_args = { "--line-length", "79" },
+        }),
+        null_ls.builtins.diagnostics.flake8.with({
+            filetypes = { "python" },
+            extra_args = { "--extend-ignore", "E203, F841" },
+        }),
 
-		-- refactoring plugin but...
-		-- null_ls.builtins.code_actions.refactoring.with({ filetypes = { "python" } }),
-		-- Lua
-		null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
-		-- Spell checking
-		-- null_ls.builtins.diagnostics.codespell.with({
-		--     args = { "--builtin", "clear,rare,code", "-" },
-		-- }),
-	},
-	on_attach = function(client, bufnr)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup_format,
-				buffer = 0,
-				callback = function()
-					vim.lsp.buf.formatting_seq_sync(nill, 20000)
-				end,
-			})
-		end
-	end,
+        -- refactoring plugin but...
+        -- null_ls.builtins.code_actions.refactoring.with({ filetypes = { "python" } }),
+        -- Lua
+        null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
+        -- Spell checking
+        -- null_ls.builtins.diagnostics.codespell.with({
+        --     args = { "--builtin", "clear,rare,code", "-" },
+        -- }),
+    },
+    on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_clear_autocmds({ buffer = 0, group = augroup_format })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup_format,
+                buffer = 0,
+                callback = function()
+                    vim.lsp.buf.formatting_seq_sync(nil, 20000)
+                end,
+            })
+        end
+    end,
 })
