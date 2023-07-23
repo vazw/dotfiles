40563bf041bbdb1f34aadcd9b101ce4dab175973
vazw <4wonzest@gmail.com>
2023-07-23 07:00:49 +0700

    fonts

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sudo apt install -y git micro zram-tools neofetch exa timeshift sway waybar swaylock swayidle swaybg swayidle lxappearance policykit-1-gnome network-manager network-manager-gnome thunar thunar-archive-plugin thunar-volman file-roller alacritty mtools dosfstools acpi acpid avahi-daemon gvfs-backends grim  grimshot slurp dunst wofi wayland-protocols xwayland libgtk-layer-shell-dev brightnessctl wl-clipboard dex jq build-essential xdg-desktop-portal-wlr kvantum libglib2.0-bin light fonts-thai-tlwg fonts-ipafont-mincho fonts-vlgothic fonts-arphic-ukai
-
+sudo apt install -y git micro zram-tools neofetch exa timeshift sway waybar swaylock swayidle swaybg swayidle lxappearance policykit-1-gnome network-manager network-manager-gnome thunar thunar-archive-plugin thunar-volman file-roller alacritty mtools dosfstools acpi acpid avahi-daemon gvfs-backends grim  grimshot slurp dunst wofi wayland-protocols xwayland libgtk-layer-shell-dev brightnessctl wl-clipboard dex jq build-essential xdg-desktop-portal-wlr kvantum libglib2.0-bin light fonts-thai-tlwg fonts-arphic-ukai fonts-arphic-uming fonts-ipafont-mincho fonts-ipafont-gothic fonts-unfonts-core
 
 sudo systemctl enable acpid
 sudo systemctl enable avahi-daemon
