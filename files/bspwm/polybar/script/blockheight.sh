fdec24464f08249c082d094061408dc6c8f6a780
vazw <vaz@siamstr.com>
2024-02-17 14:13:51 +0700

    del old files from X11

--- files/bspwm/polybar/script/blockheight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-price=$(curl -sX GET "https://blockchain.info/q/getblockcount" -H  "accept: application/json" )
-
-if [[ ! $price -eq "" ]]; then
-    echo "$price" > blockheight
-fi
-cat blockheight
