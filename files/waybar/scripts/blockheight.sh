8875ec771138537d3605b2be52709ba3ab202d7a
vazw <vaz@siamstr.com>
2024-05-29 15:23:15 +0700

    update fail safe script

--- files/waybar/scripts/blockheight.sh
+++ files/waybar/scripts/blockheight.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-price=$(curl -sX GET "https://blockchain.info/q/getblockcount" -H  "accept: application/json" )
+price=$(curl -sX GET "https://blockchain.info/q/getblockcount" -H  "accept: application/json")
 
-if [[ ! $price -eq "" ]]; then
+if [[ -n $price && $price -gt 0 ]] ; then
     echo "$price" > "$HOME/.config/waybar/scripts/blockheight"
 fi
 cat "$HOME/.config/waybar/scripts/blockheight"
