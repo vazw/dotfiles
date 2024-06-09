35cb4841966c0fcbb0c24f2119846ece9d4faefc
vazw <vaz@siamstr.com>
2024-06-09 21:56:03 +0700

    update nvim

--- files/nvim/lua/settings/plugins/treesister.lua
+++ files/nvim/lua/settings/plugins/treesister.lua
@@ -1,50 +1,50 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	priority = 1000,
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
-		require("ts_context_commentstring").setup({})
-		-- configure treesitter
-		treesitter.setup({
-			-- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = { enable = true },
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"python",
-				"rust",
-			},
-			-- auto install above language parsers
-			auto_install = true,
-		})
-		vim.g.skip_ts_context_commentstring_module = true
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    priority = 1000,
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+        require("ts_context_commentstring").setup({})
+        -- configure treesitter
+        treesitter.setup({
+            -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- enable autotagging (w/ nvim-ts-autotag plugin)
+            autotag = { enable = true },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "prisma",
+                "markdown",
+                "markdown_inline",
+                "svelte",
+                "graphql",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "python",
+                "rust",
+            },
+            -- auto install above language parsers
+            auto_install = true,
+        })
+        vim.g.skip_ts_context_commentstring_module = true
+    end,
 }
---
+++ files/nvim/lua/settings/plugins/treesitterrstml.lua
@@ -0,0 +1,8 @@
+return {
+	-- "rayliwell/tree-sitter-rstml",
+	-- dependencies = { "nvim-treesitter" },
+	-- build = ":TSUpdate",
+	-- config = function()
+	-- 	require("tree-sitter-rstml").setup()
+	-- end,
+}
