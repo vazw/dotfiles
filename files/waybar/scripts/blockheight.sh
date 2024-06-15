9a8a8599136cc5e4db453caa21496cfe0169527c
vazw <vaz@siamstr.com>
2024-06-16 01:25:06 +0700

    simple waybar :D

--- files/waybar/scripts/blockheight.sh
+++ files/waybar/scripts/blockheight.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+price="$(wget 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT' -q -O - | jq -r .price)"
+rounded_Price=$(printf "%'.0f\n" "${price}")
+if [[ ! $rounded_Price -eq 0 ]]; then
+    echo "" "${rounded_Price}" > "$HOME/.config/waybar/scripts/BTCUSD"
+fi
+
 price=$(curl -sX GET "https://blockchain.info/q/getblockcount" -H  "accept: application/json")
 
 if [[ -n $price && $price -gt 0 ]] ; then
     echo "$price" > "$HOME/.config/waybar/scripts/blockheight"
 fi
-cat "$HOME/.config/waybar/scripts/blockheight"
+
+block=$(cat "$HOME/.config/waybar/scripts/blockheight")
+btc_price=$(cat "$HOME/.config/waybar/scripts/BTCUSD")
+
+printf "$block\n$btc_price"
