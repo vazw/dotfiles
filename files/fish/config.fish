710d3bb787b5ffe22e3b83b7167bf7dcc3a88616
vazw <vaz@siamstr.com>
2025-06-20 16:22:40 +0700

    update fish

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -196,8 +196,8 @@ set -x MANPAGER 'nvim +Man!'
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons' # all files and dirs
+# alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+# alias la='exa -a --color=always --group-directories-first --icons' # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons' # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
@@ -247,6 +247,7 @@ alias free='free -h'
 # Dotfiles & Files
 alias gc="git clone"
 alias reboot="loginctl reboot"
+alias waydroid-reboot="sudo waydroid container restart"
 alias wttr="curl wttr.in"
 
 # Cleanup orphaned packages
