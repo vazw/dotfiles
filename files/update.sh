944c858e62bf25b2145ce53d222dfd28b80d6be3
vazw <vaz@siamstr.com>
2025-07-02 15:45:17 +0700

    minimize tmux

--- files/update.sh
+++ files/update.sh
@@ -7,11 +7,11 @@ echo "Ignored: $IGNORE"
 for UPDATE_PATH in "${FILES[@]}"; do
 	if [[ ! " ${IGNORE[*]} " =~ [[:space:]]$UPDATE_PATH[[:space:]] ]]; then
 		if [[ -f $UPDATE_PATH ]]; then
-			cp "$HOME/.config/$UPDATE_PATH" .
+			cp -u "$HOME/.config/$UPDATE_PATH" .
 			echo "Copied $UPDATE_PATH"
 		fi
 		if [[ -d $UPDATE_PATH ]]; then
-			cp -r "$HOME/.config/$UPDATE_PATH" .
+			cp -ru "$HOME/.config/$UPDATE_PATH" .
 			echo "Updated $UPDATE_PATH"
 		fi
 	fi
